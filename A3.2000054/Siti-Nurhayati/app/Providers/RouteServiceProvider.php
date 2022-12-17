362718da3966c8fb29b83c93f5d76a1d825dc861
sitinhaaa <siti98365@gmail.com>
2022-12-17 15:44:02 +0700

    Link Website UAS

--- A3.2000054/Siti-Nurhayati/app/Providers/RouteServiceProvider.php
+++ A3.2000054/Siti-Nurhayati/app/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\Http\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -34,21 +34,21 @@ class RouteServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        $this->configureRateLimiting();
+{
+  $this->configureRateLimiting();
 
-        $this->routes(function () {
-            Route::prefix('api')
-                ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+  $this->routes(function () {
+      Route::middleware('web')
+        ->namespace($this->namespace) 
+        ->group(base_path('routes/web.php'));
 
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
-        });
-    }
 
+      Route::prefix('api')
+        ->namespace($this->namespace)  
+        ->middleware('api')
+        ->group(base_path('routes/api.php'));
+ });
+}
     /**
      * Configure the rate limiters for the application.
      *
